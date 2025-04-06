1534e732aba6777d1476c8575808c68354add6bc
sergey <wendingnn@gmail.com>
2025-04-06 15:08:46 +0300

    init repository

---
+++ src/test/java/goods/example/goods/GoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package goods.example.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
